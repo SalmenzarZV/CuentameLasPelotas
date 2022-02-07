fb9ce7bc15ed631e55af19cf9ff4645bbf932658
Salmenzar <salmenzar1510@ieszaidinvergeles.org>
2022-02-07 10:10:30 +0100

    empezar metodo paintballs

--- app/src/main/java/es/saj/cuentamelaspelotas/Ball.java
+++ app/src/main/java/es/saj/cuentamelaspelotas/Ball.java
@@ -10,16 +10,58 @@ import java.util.concurrent.ThreadLocalRandom;
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class Ball {
     private static final int RADIO = 30 ;
-    final int[] colors = {Color.RED, Color.BLUE, Color.YELLOW, Color.GREEN, Color.BLACK, Color.MAGENTA};
-
-    int w, h, randomBalls, randomX, randomY, color;
+    int w, h, randomX, randomY, color;
 
 
-    public Ball(int width, int height) {
+    public Ball(int width, int height, int color) {
         w = width - RADIO;
         h = height - RADIO;
-        randomBalls = ThreadLocalRandom.current().nextInt(2, 6);//OJO, RANGO ENTRE 2 Y 6
-        color = colors[ThreadLocalRandom.current().nextInt(0, colors.length-1)];
+        randomX = ThreadLocalRandom.current().nextInt(RADIO, w);
+        randomY = ThreadLocalRandom.current().nextInt(RADIO, h);;
+        this.color = color;
     }
 
+    public static int getRADIO() {
+        return RADIO;
+    }
+
+    public int getW() {
+        return w;
+    }
+
+    public void setW(int w) {
+        this.w = w;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+
+    public int getRandomX() {
+        return randomX;
+    }
+
+    public void setRandomX(int randomX) {
+        this.randomX = randomX;
+    }
+
+    public int getRandomY() {
+        return randomY;
+    }
+
+    public void setRandomY(int randomY) {
+        this.randomY = randomY;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
 }
--- app/src/main/java/es/saj/cuentamelaspelotas/GraphicView.java
+++ app/src/main/java/es/saj/cuentamelaspelotas/GraphicView.java
@@ -5,19 +5,21 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
+import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class GraphicView extends View {
     Paint text;
-    Paint[] balls;
+    Paint[] paintBalls;
+    ArrayList<int[]> balls;
 
+    final int RADIO = 30 ;
     int initPosText = getHeight()+ 32,
         textSpeed = 13,
         difficulty = 0;
-    int posIniX, posIniY, centroX, centroY, xPeed, ySpeed;
+    int centroX, centroY;
     int[] colors = {Color.BLUE, Color.BLACK, Color.RED, Color.GREEN, Color.MAGENTA, Color.YELLOW};
 
     public GraphicView(Context context, AttributeSet attrs) {
@@ -25,6 +27,7 @@ public class GraphicView extends View {
         text = new Paint();
         text.setColor(Color.BLUE);
         text.setTextSize(100);
+        createBalls(difficulty);
     }
 
     @Override
@@ -66,33 +69,80 @@ public class GraphicView extends View {
 
     //TODO COMPROBAR QUE EL ARRAY DE PELOTAS ESTA INICIALIZADO
     private void paintBalls(Canvas c) {
-        if (balls == null || balls.length == 0){
-            createBalls(difficulty);
-        }
 
     }
 
     private void createBalls(int difficulty) {
+        int[] colors;
+        int nBalls;
         switch (difficulty){
             case 1:/*Easy Mode*/  //Log.v("jamaica", "izi mode");
-                iziMode();
+                colors = chooseColors(2);
+                nBalls = ThreadLocalRandom.current().nextInt(2, 4);
+                balls = inflateBalls(colors, nBalls);
                 break;
             case 2:/*Normal Mode*/ //Log.v("jamaica", "normalelelemode");
-                normaleMode();
+                colors = chooseColors(ThreadLocalRandom.current().nextInt(0, this.colors.length));
+                nBalls = ThreadLocalRandom.current().nextInt(4, 7);
+                balls = inflateBalls(colors, nBalls);
                 break;
             case 3:/*Hard Mode*/ //Log.v("jamaica", "Dificilisimode");
-                dificilisiMode();
+                colors = chooseColors(0);
+                nBalls = ThreadLocalRandom.current().nextInt(7, 11);
+                balls = inflateBalls(colors, nBalls);
                 break;
         }
+
+        paintBalls = new Paint[balls.size()];
+    }
+
+    private ArrayList<int[]> inflateBalls(int[] colors, int nBalls) {
+        int randomX, randomY, color;
+        int[] ball;
+        ArrayList<int[]> balls= new ArrayList<int[]>();
+        for (int i = 0; i<nBalls; i++){
+            randomX = ThreadLocalRandom.current().nextInt(RADIO, getWidth());
+            randomY = ThreadLocalRandom.current().nextInt(RADIO, getHeight());
+            color = colors[ThreadLocalRandom.current().nextInt(0, colors.length)];
+            ball = new int[] {randomX, randomY, color};
+            balls.add(ball);
+        }
+        return balls;
+    }
+
+    private int[] chooseColors(int numColors) {
+        int[] colors;
+        if (numColors == 0){
+            colors = this.colors;
+        } else {
+            ArrayList<Integer> addColors= new ArrayList<>();
+
+            for (int i = 0; i<numColors; i++){
+                int color = ThreadLocalRandom.current().nextInt(0,this.colors.length);
+                if (addColors.contains(color)){
+                    i--;
+                } else {
+                    addColors.add(color);
+                }
+            }
+            colors = new int[addColors.size()];
+            for (int i = 0; i<colors.length; i++){
+                colors[i] = addColors.get(i);
+            }
+        }
+
+        return colors;
+    }
+
+    private void normaleMode() {
+
     }
 
     private void dificilisiMode() {
 
     }
 
-    private void normaleMode() {
 
-    }
 
     private void iziMode() {
         int color1 = ThreadLocalRandom.current().nextInt(0, colors.length), color2;
@@ -103,6 +153,8 @@ public class GraphicView extends View {
         }while (color2 == color1);
 
         int numBalls = ThreadLocalRandom.current().nextInt(2, 4);
+
+
     }
 
     public void setDifficulty(int difficulty) {
